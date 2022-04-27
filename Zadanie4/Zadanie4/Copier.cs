72bca6f19f500486e1b004a3a1bac512b5870b55
K4thos <piotrpit.uek@gmail.com>
2022-04-28 00:12:37 +0200

    Excercise 4 solution

---
+++ Zadanie4/Zadanie4/Copier.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Threading;
+
+namespace Zadanie4
+{
+    public class Copier : IPrinter, IScanner
+    {
+        public int PrintCounter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+        public int Counter { get; private set; } = 0;
+
+        protected IDevice.State state = IDevice.State.off;
+
+        public IDevice.State GetState()
+        {
+            if (state == IDevice.State.off)
+            {
+                return IDevice.State.off;
+            }
+            if (IPrinter.PrinterState == IDevice.State.standby && IScanner.ScannerState == IDevice.State.standby)
+            {
+                return IDevice.State.standby;
+            }
+            return IDevice.State.on;
+        }
+
+        void IDevice.SetState(IDevice.State state)
+        {
+            if (state == IDevice.State.on && this.state == IDevice.State.off)
+            {
+                Counter++;
+            }
+            this.state = state;
+            IPrinter.PrinterState = state;
+            IScanner.ScannerState = state;
+        }
+
+        public void StandbyOn()
+        {
+            this.state = IDevice.State.standby;
+            IPrinter.PrinterState = IDevice.State.standby;
+            IScanner.ScannerState = IDevice.State.standby;
+        }
+
+        public void StandbyOff()
+        {
+            this.state = IDevice.State.on;
+            IPrinter.PrinterState = IDevice.State.on;
+            IScanner.ScannerState = IDevice.State.on;
+        }
+
+        public void PowerOn()
+        {
+            if (GetState() == IDevice.State.off)
+            {
+                Counter++;
+                this.state = IDevice.State.on;
+                IPrinter.PrinterState = IDevice.State.on;
+                IScanner.ScannerState = IDevice.State.on;
+            }
+        }
+
+        public void PowerOff()
+        {
+            this.state = IDevice.State.off;
+            IPrinter.PrinterState = IDevice.State.off;
+            IScanner.ScannerState = IDevice.State.off;
+        }
+
+        public void Print(in IDocument document)
+        {
+            if (IPrinter.PrinterState == IDevice.State.off)
+            {
+                return;
+            }
+
+            IPrinter.PrinterState = IDevice.State.on;
+
+            if (PrintCounter > 0 && PrintCounter % 3 == 0)
+            {
+                Console.WriteLine("Printer is cooling down for 1 sec");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+
+            PrintCounter++;
+            IPrinter.PrinterState = IDevice.State.standby;
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        {
+            if (IScanner.ScannerState == IDevice.State.off)
+            {
+                document = null;
+                return;
+            }
+
+            IScanner.ScannerState = IDevice.State.on;
+
+            if (ScanCounter > 0 && ScanCounter % 2 == 0)
+            {
+                Console.WriteLine("Scanner is cooling down for 1 sec");
+                Thread.Sleep(1000);
+            }
+
+            switch (formatType)
+            {
+                case IDocument.FormatType.JPG:
+                    document = new ImageDocument($"ImageScan{ScanCounter}.jpg");
+                    break;
+                case IDocument.FormatType.PDF:
+                    document = new ImageDocument($"PDFScan{ScanCounter}.pdf");
+                    break;
+                case IDocument.FormatType.TXT:
+                    document = new ImageDocument($"TextScan{ScanCounter}.txt");
+                    break;
+                default:
+                    throw new ArgumentException("File type has not been defined");
+            }
+
+            Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+
+            ScanCounter++;
+
+            IScanner.ScannerState = IDevice.State.standby;
+        }
+
+        public void ScanAndPrint()
+        {
+            if (state == IDevice.State.on)
+            {
+                IDocument document;
+                Scan(out document, IDocument.FormatType.JPG);
+                Print(document);
+            }
+        }
+    }
+}
---
+++ Zadanie4/Zadanie4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main()
+        {
+            var xerox = new Copier();
+            Console.WriteLine($"Copier state: {xerox.GetState()}");
+            xerox.PowerOn();
+            Console.WriteLine($"Copier state: {xerox.GetState()}");
+
+            var doc = new PDFDocument("aaa.pdf");
+            xerox.Print(doc);
+            xerox.Print(doc);
+            xerox.Print(doc);
+            xerox.Print(doc);
+            xerox.Print(doc);
+
+            IDocument doc1;
+            IDocument doc2;
+            IDocument doc3;
+            xerox.Scan(out doc1);
+            xerox.Scan(out doc2);
+            xerox.Scan(out doc3);
+
+            Console.WriteLine($"Copier counter: {xerox.Counter}");
+            Console.WriteLine($"Scan counter: {xerox.ScanCounter}");
+            Console.WriteLine($"Print counter: {xerox.PrintCounter}");
+
+            Console.WriteLine($"Copier state: {xerox.GetState()}");
+            xerox.PowerOff();
+            Console.WriteLine($"Copier state: {xerox.GetState()}");
+        }
+    }
+}
