9c32a4071db0cb40fd6fea1aec52477677400848
K4thos <piotrpit.uek@gmail.com>
2022-04-27 14:08:37 +0200

    Excercise 1 solution

---
+++ Zadanie1/Zadanie1/Copier.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+        public new int Counter { get; private set; } = 0;
+
+        public new void PowerOn()
+        {
+            if (GetState() == IDevice.State.off)
+            {
+                Counter++;
+                base.PowerOn();
+            }
+        }
+
+        public new void PowerOff()
+        {
+            if (GetState() == IDevice.State.on)
+            {
+                base.PowerOff();
+            }
+        }
+
+        public void Print(in IDocument document)
+        {
+            if (GetState() == IDevice.State.on)
+            {
+                Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+                PrintCounter++;
+            }
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        {
+            if (GetState() == IDevice.State.off)
+            {
+                document = null;
+                return;
+            }
+
+            ScanCounter++;
+
+            switch (formatType)
+            {
+                case IDocument.FormatType.JPG:
+                    document = new ImageDocument($"ImageScan{ScanCounter}.jpg");
+                    break;
+                case IDocument.FormatType.PDF:
+                    document = new ImageDocument($"PDFScan{ScanCounter}.pdf");
+                    break;
+                case IDocument.FormatType.TXT:
+                    document = new ImageDocument($"TextScan{ScanCounter}.txt");
+                    break;
+                default:
+                    throw new ArgumentException("File type has not been defined");
+            }
+
+            Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+        }
+
+        public void ScanAndPrint()
+        {
+            if (state == IDevice.State.on)
+            {
+                IDocument document;
+                Scan(out document, IDocument.FormatType.JPG);
+                Print(document);
+            }
+        }
+    }
+}
